e3f373d3bb1aba4be41325e74c9d8d9506b1f912
Azaliya <ziganshina.azaliya1@gmail.com>
2026-03-19 12:00:04 +0300

    Вариант 2

--- src/main/java/com/example/lms/exception/StudentNotFoundException.java
+++ src/main/java/com/example/lms/exception/StudentNotFoundException.java
@@ -2,9 +2,9 @@ package com.example.lms.exception;
 
 public class StudentNotFoundException extends NotFoundException {
     public StudentNotFoundException(Long id) {
-        super("Student with id = " + id + " not found");
+       super("Student with id = " + id + " not found");
     }
     public StudentNotFoundException() {
-        super("Students not found");
+       super("Students not found");
     }
 }
